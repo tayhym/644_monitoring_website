2ac58d501a13c05028368629f7fd1b8999b1ecc2
tayhym <tayhym@gmail.com>
2014-10-21 22:18:02 -0400

    initial commit
    general outline, bootstrap files, sample pictures, all deployed to
    heroku.

---
+++ hello/static/hello/js/tabs_interactive.js
@@ -0,0 +1,4 @@
+$('#tab_id a').click(function (e) {
+  e.preventDefault()
+  $(this).tab('show')
+})
---
+++ venv/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/Users/matthew/Documents/18-644/Team_Webpage/python-getting-started/venv/bin/python
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
