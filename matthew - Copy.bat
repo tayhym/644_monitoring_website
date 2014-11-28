ad93c8637528ca31d443bc5c8a4ea2ad7e19d62e
unknown <krov.clement@gmail.com>
2014-11-28 17:35:02 -0500

    'photo_upload'

---
+++ matthew - Copy.bat
@@ -0,0 +1,12 @@
+:loop
+    matlab -r compile -nosplash -nodesktop -nodisplay
+    timeout /t 20 > null
+    taskkill /f /im MATLAB.exe
+    
+	echo 'searching for new photos to push'
+	git add .
+	git commit -m 'photo_upload' 
+	git push 
+	git push heroku master
+	sleep 60
+goto loop
